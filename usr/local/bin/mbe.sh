6330c0db53641da9f7141293a337b02b8c17f2ac
マリウス <marius@xn--gckvb8fzb.com>
2022-07-31 00:27:55 -0500

    Updated mbe.sh

--- usr/local/bin/mbe.sh
+++ usr/local/bin/mbe.sh
@@ -12,6 +12,7 @@
 # ║                                                                            ║
 # ╚════════════════════════════════════════════════════════════════════════════╝
 source ~/.secrets
+MBE_PASSWORD=$(pass show mbe)
 
 curl -s 'https://mbe-latam.com/eboxweb/?lang=en' \
   -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36' \
