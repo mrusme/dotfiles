4bb93c76af0f8ad610d446b600f98b49ed7018ef
マリウス <marius@xn--gckvb8fzb.com>
2022-08-30 14:52:47 -0500

    Migrated from wofi to bemenu

--- usr/local/bin/cpu.sh
+++ usr/local/bin/cpu.sh
@@ -4,7 +4,7 @@ cmd=$1
 
 if [[ "$cmd" == "set" ]]
 then
-  governor=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors | tr " " "\n" | grep -v '^$' | wofi --dmenu)
+  governor=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors | tr " " "\n" | grep -v '^$' | bemenu)
   if [[ "$governor" != "" ]]
   then
     sudo cpupower frequency-set -g "$governor"
