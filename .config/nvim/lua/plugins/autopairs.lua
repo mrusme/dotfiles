081a4742788a694109138ff48185efa37855594f
マリウス <marius@xn--gckvb8fzb.com>
2025-12-14 20:17:13 +0100

    Add config

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -2,6 +2,13 @@ return {
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    config = true,
+    config = function()
+      require("nvim-autopairs").setup({
+        check_ts = true,
+        map_cr = true,
+        map_complete = true,
+        disable_filetype = { "TelescopePrompt", "vim" },
+      })
+    end,
   },
 }
